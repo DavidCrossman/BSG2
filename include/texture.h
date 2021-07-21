94d5e3023fd04cd2a931698ef08fb3ef6cd43932
DavidCrossman <22548398+DavidCrossman@users.noreply.github.com>
2021-07-21 20:30:53 +0100

    Texture properties can now be set

--- include/texture.h
+++ include/texture.h
@@ -8,5 +8,9 @@ public:
     GLuint id;
     Texture(GLuint id);
     ~Texture();
+    void set_mag_filter(GLuint mag_filter);
+    void set_min_filter(GLuint min_filter);
+    void set_horizontal_wrap(GLuint horizontal_wrap);
+    void set_verical_wrap(GLuint vertical_wrap);
 };
 }
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,11 +1,32 @@
 #include "texture.h"
 
 namespace bsg2 {
-Texture::Texture(GLuint id) : id(id) {
-    glBindTexture(GL_TEXTURE_2D, id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-}
+Texture::Texture(GLuint id) : id(id) {}
 
 Texture::~Texture() { glDeleteTextures(1, &id); }
+
+void Texture::set_mag_filter(GLuint mag_filter) {
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::set_min_filter(GLuint min_filter) {
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::set_horizontal_wrap(GLuint horizontal_wrap) {
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, horizontal_wrap);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::set_verical_wrap(GLuint vertical_wrap) {
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, vertical_wrap);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 }
