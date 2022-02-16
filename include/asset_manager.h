1bfca923e291c61a6347e8a12ef046d3614cacd5
DavidCrossman <davidocrossman@gmail.com>
2022-02-16 16:31:01 +0000

    Texture class functions are now non-const

--- include/asset_manager.h
+++ include/asset_manager.h
@@ -11,28 +11,36 @@ public:
     AssetManager(const std::string& asset_dir = "assets/") {
         set_asset_dir(asset_dir);
     }
+
     AssetManager(const AssetManager& other) = delete;
+
     ~AssetManager() {
-        for (const auto& kv : assets) {
-            delete kv.second;
+        for (const auto& [path, asset] : assets) {
+            delete asset;
         }
     }
-    const std::string& asset_dir() const {
-        return m_asset_dir;
+
+    const std::string& get_asset_dir() const {
+        return asset_dir;
     }
+
     void set_asset_dir(const std::string& asset_dir) {
-        m_asset_dir = find_full_dir(asset_dir);
+        this->asset_dir = find_full_dir(asset_dir);
     }
+
     virtual void load(std::string path) = 0;
+
     T& get(const std::string& path) {
         return *assets.find(path)->second;
     }
+
     T& load_get(const std::string& path) {
         if (assets.find(path) == assets.end()) load(path);
         return *assets.find(path)->second;
     }
+
 protected:
     std::unordered_map<std::string, T*> assets;
-    std::string m_asset_dir;
+    std::string asset_dir;
 };
 }
--- src/texture.cpp
+++ src/texture.cpp
@@ -9,25 +9,25 @@ Texture::~Texture() {
     glDeleteTextures(1, &id);
 }
 
-void Texture::set_mag_filter(GLuint mag_filter) const {
+void Texture::set_mag_filter(GLuint mag_filter) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-void Texture::set_min_filter(GLuint min_filter) const {
+void Texture::set_min_filter(GLuint min_filter) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-void Texture::set_horizontal_wrap(GLuint horizontal_wrap) const {
+void Texture::set_horizontal_wrap(GLuint horizontal_wrap) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, horizontal_wrap);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-void Texture::set_vertical_wrap(GLuint vertical_wrap) const {
+void Texture::set_vertical_wrap(GLuint vertical_wrap) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, vertical_wrap);
     glBindTexture(GL_TEXTURE_2D, 0);
@@ -36,7 +36,7 @@ void Texture::set_vertical_wrap(GLuint vertical_wrap) const {
 TextureManager::TextureManager(const std::string& asset_dir) : AssetManager(asset_dir) {}
 
 void TextureManager::load(std::string path) {
-    std::string qualified_path = m_asset_dir + path;
+    std::string qualified_path = asset_dir + path;
     assets.emplace(path, new Texture(SOIL_load_OGL_texture(qualified_path.c_str(), SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID,
         SOIL_FLAG_GL_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT)));
 }
