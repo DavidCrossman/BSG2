c9424ce054c13d3221b7bed01c7885fb5fb397c6
DavidCrossman <davidocrossman@gmail.com>
2022-09-09 18:53:36 +0100

    BSG2 is now installed inside the project directory

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -1,2 +1,2 @@
-cmake -B linux-out/Release -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
-cmake -B linux-out/Debug -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
+cmake -B out/build/linux-Release -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
+cmake -B out/build/linux-Debug -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
