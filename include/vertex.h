f4ce66c2289bb5cf723f7a9015d9c8997e033c65
DavidCrossman <22548398+DavidCrossman@users.noreply.github.com>
2025-12-01 16:56:19 +0000

    Add default constructor to `Vertex`

--- include/vertex.h
+++ include/vertex.h
@@ -7,6 +7,7 @@ namespace bsg2 {
 struct Vertex {
 	inline Vertex(glm::vec2 pos, glm::vec4 colour, glm::vec2 tex_coords, float depth = 0) : 
 		pos(pos), colour(colour), tex_coords(tex_coords), depth(depth) {}
+	inline Vertex() {}
 	glm::vec2 pos;
 	float depth;
 	glm::vec4 colour;
